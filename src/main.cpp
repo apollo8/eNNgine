571c1ec55c81dbe5cbb04268babc9226bbcfd6ca
Benjamin Bittner <benjamin_bittner@gmx.it>
2020-02-22 01:46:42 +0100

    add test calls for new mnist functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 #include "demos.hpp"
-#include "mnist_helpers.hpp"
+#include "mnist/mnist_reader.hpp"
 
 
 int main(int argc, char** argv)
@@ -13,9 +13,12 @@ int main(int argc, char** argv)
     //run_sigmoid_neuron_demo();
     //convert_decimal_to_binary_demo();
 
-    auto images = read_mnist_images("/Users/benjamin/Documents/data_sets/mnist/train-images-idx3-ubyte");
+    auto mnist_dataset = mnist::read_dataset("/Users/benjamin/Documents/data_sets/mnist/", 0, 0);
 
-    std::cout << "#images: " << images.size() << std::endl;
+    std::cout << "training_images.size() = " << mnist_dataset.training_images.size() << std::endl;
+    std::cout << "training_labels.size() = " << mnist_dataset.training_labels.size() << std::endl;
+    std::cout << "test_images.size() = " << mnist_dataset.test_images.size() << std::endl;
+    std::cout << "test_labels.size() = " << mnist_dataset.test_labels.size() << std::endl;
 
     return 0;
 }
